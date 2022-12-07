4841bf8b4cd05893e095d45e09182ddb91afc429
Danny Fehrenbach <dfehrenbach04@gmail.com>
2022-12-07 14:40:09 -0600

    Refactor day 7 to use a simpler structure
    values are either maps or integers. integer when file. keys are always name of dirs or files
    
    could go EVEN simpler and flatten the structure by representing directory paths as the keys of a map and their values being just the number. as one reads in files, you simply update the structure adding in the new size

--- src/day7/core.clj
+++ src/day7/core.clj
@@ -6,97 +6,73 @@
        slurp
        string/split-lines))
 
-(def init-system {"/" {:size 0 :children {} :type :dir}})
-
-;; keep track of dir path
-;; [/ a b c] as /a/b/c and uncover file sizes
-;; Going with -> THIS ONE: can either build as you go by taking all discovered files and adding to total on each path
-;; or build numbers at the end doing one level at a time from the bottom up
-
-(defn init-dir [system path instruction]
-  (let [name (second (string/split instruction #" "))]
-    (assoc-in system (concat (interleave path (repeat :children)) [name])
-              {:size 0 :children {} :type :dir :name name})))
-
-(defn init-file [system path size name]
-  (assoc-in system (concat (interleave path (repeat :children)) [name])
-            {:size size :type :file :name name}))
-
-(defn cascade-file-size-upwards [system path size]
+(defn cascade-sizes [system path size]
   (let [all-paths (rest (reductions conj [] path))]
-    (reduce (fn [acc-system this-path]
-              (update-in acc-system
-                         (concat (butlast (interleave this-path (repeat :children))) [:size])
-                         + size))
+    (reduce (fn [sys p]
+              (let [current-size (get-in sys (conj p :size) 0)]
+                (assoc-in sys (conj p :size) (+ (parse-long size) current-size))))
             system all-paths)))
 
-(defn parse-cd-path [instruction path]
-  (let [name (nth (string/split instruction #" ") 2)]
-    (case name
-      "/" ["/"]
-      ".." (pop path)
-      (conj path name))))
-
-(defn add-in-new-file [system path instruction]
-  (let [[size-str name] (string/split instruction #" ")]
-    (-> system
-        (init-file path (parse-long size-str) name)
-        (cascade-file-size-upwards path (parse-long size-str)))))
-
-(defn read-instructions [input]
-  (loop [instructions input
-         system init-system
+(defn read-instructions3 [input]
+  (loop [cmds input
+         system {}
          path []]
-    (if (empty? instructions) system
-        (let [[instruction & rest-instructions] instructions]
+    (if (empty? cmds) system
+        (let [cmd (first cmds)]
           (cond
-            (string/starts-with? instruction "$ cd")
-            (recur rest-instructions system (parse-cd-path instruction path))
+            (string/starts-with? cmd "$ ls")
+            (recur (rest cmds) system path)
 
-            (string/starts-with? instruction "$ ls")
-            (recur rest-instructions system path)
+            (string/starts-with? cmd "$ cd /")
+            (recur (rest cmds) system ["/"])
 
-            (string/starts-with? instruction "dir")
-            (recur rest-instructions (init-dir system path instruction) path)
+            (string/starts-with? cmd "$ cd ..")
+            (recur (rest cmds) system (pop path))
 
-            (re-matches #"\d.*" instruction)
-            (recur rest-instructions (add-in-new-file system path instruction) path))))))
+            (string/starts-with? cmd "$ cd")
+            (recur (rest cmds) system (conj path (nth (string/split cmd #" ") 2)))
 
-;;Then it's about walking the tree to find directory sizes that dip under
-(defn all-directories [system]
-  (filter #(and (map? %)
-                (contains? % :type)
-                (= :dir (:type %)))
-          (tree-seq map? vals system)))
+            (string/starts-with? cmd "dir")
+            (recur (rest cmds) system path)
 
-(defn part1 [input]
-  (->> input
-       read-instructions
-       all-directories
+            (re-matches #"(\d+) (.*)" cmd)
+            (let [[_ size name] (re-matches #"(\d+) (.*)" cmd)]
+              (recur (rest cmds)
+                     (-> system
+                         (update-in path assoc name (parse-long size))
+                         (cascade-sizes path size))
+                     path)))))))
+
+(defn all-directory-sizes [system]
+  (->> system
+       (tree-seq map? vals)
        (map :size)
+       (remove nil?)))
+
+(defn part11 [input]
+  (->> input
+       read-instructions3
+       all-directory-sizes
        (filter (partial >= 100000))
        (reduce +)))
 
-(defn part2 [input]
-  (let [full-system (read-instructions input)
+(defn part22 [input]
+  (let [full-system (read-instructions3 input)
         used-space (get-in full-system ["/" :size])]
     (->> full-system
-         all-directories
-         (map :size)
-         (filter #(<= 30000000 (+ % (- 70000000 used-space))))
+         all-directory-sizes
+         (filter #(<= (- used-space (- 70000000 30000000)) %))
          (apply min))))
+
 
 (comment
   (take 5 input)
   ;; => ("$ cd /" "$ ls" "dir ddpgzpc" "dir mqjrd" "dir mrqjg")
 
-  (nth (->> input read-instructions all-directories) 5)
-  ;; => {:size 74360, :children {"zjq" {:size 74360, :type :file, :name "zjq"}}, 
-  ;;     :type :dir, :name "dtgnbb"}
-
-  (part1 input)
+  (part11 input)
   ;; => 1915606
 
-  (part2 input)
+  (part22 input)
   ;; => 5025657
-  )
+
+  0)
